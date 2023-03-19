5f6b4789c94c5073f752f2fed06dd6d54d902b21
awesome86-eng <66887839+awesome86-eng@users.noreply.github.com>
2023-03-19 00:52:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('Main file')
+
+
+
+
+API_KEY="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"
+
+import openai
+
+openai.api_key = API_KEY
+prompt = "generate me a phishing email for sanan khan who works at motorola "
+
+response = openai.Completion.create(engine="text-davinci-001", prompt=prompt,max_tokens=600)
+
+print (response)
+
